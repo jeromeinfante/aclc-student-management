781dd0b1edb30178aa9ee720f34dc04b38a0f24a
Boboy Infante <jeromeinfante0506@gmail.com>
2023-11-10 11:20:15 +0800

    added comments and styles htmls

--- src/dbconn.php
+++ src/dbconn.php
@@ -1,6 +1,22 @@
 <?php
 
-$CONN = mysqli_connect("localhost", "root", "", "REGISTRATION_PAGE");
+/*
+
+MA GIBO MUNA KITA DATABASE, NAME: {BOBOY}
+DB NAME: users
+id: ai check int;
+full_name: varchar 255;
+email_address: varchar 255;
+user_password: varchar 255;
+
+
+
+*/
+
+
+
+
+$CONN = mysqli_connect("localhost", "root", "", "BOBOY"); //INI SI NAME KANG DATABASE SA PHPMYADMIN
 
 if (!$CONN) {
     die("". mysqli_connect_error());
