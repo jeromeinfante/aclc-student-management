84f510359b6d518a8038b39ae5314b34010c91bb
Boboy Infante <jeromeinfante0506@gmail.com>
2023-11-11 20:26:49 +0800

    added images and added gallery

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,23 @@
+// GITHUB BUTTON
 let github_button = document.getElementById("github_button");
 
 github_button.addEventListener("click", () => {
 	window.open("https://github.com/sphyxi4");
 });
+// END GITHUB BUTTON
+
+// GALLERY CLICK FUNCTIOn
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
