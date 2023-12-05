3ac74b774f49300727a2b34d0b9c567795a75934
sphyxi4 <jeromeinfante0506@gmail.com>
2023-12-05 09:51:17 +0800

    .

--- app/add.php
+++ app/add.php
@@ -55,19 +55,19 @@ if (isset($_SESSION['admin_username'])) {
                             </div>
                             <div class="input-box">
                                 <span class="details">Phone Number</span>
-                                <input type="text" placeholder="Enter your Phone Number" required name="CP_NUMBER">
+                                <input type="number" placeholder="Enter your Phone Number" required name="CP_NUMBER">
                             </div>
                             <div class="input-box">
                                 <span class="details">Hometown</span>
-                                <input type="text" placeholder="Enter your number" required name="HOMETOWN">
+                                <input type="text" placeholder="Enter your Hometown" required name="HOMETOWN">
                             </div>
                         </div>
                         <h4>Grade Level</h4>
                         <div class="Grade">
-                            <div class="item"><input type="radio" name="grade_level" value="11">
+                            <div class="item"><input type="radio" name="grade_level" value="11" required>
                                 <label for="11">11</label>
                             </div>
-                            <div class="item"><input type="radio" name="grade_level" value="12">
+                            <div class="item"><input type="radio" name="grade_level" value="12" required>
                                 <label for="12">12</label>
                             </div>
 
@@ -75,63 +75,63 @@ if (isset($_SESSION['admin_username'])) {
                         <h4>Section</h4>
                         <div class="Section">
                             <div class="item">
-                                <input type="radio" name="grade_section" value="A">
+                                <input type="radio" name="grade_section" value="A" required>
                                 <label for="11">A</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="B">
+                                <input type="radio" name="grade_section" value="B" required>
                                 <label for="12">B</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="C">
+                                <input type="radio" name="grade_section" value="C" required>
                                 <label for="12">C</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="D">
+                                <input type="radio" name="grade_section" value="D" required>
                                 <label for="12">D</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="E">
+                                <input type="radio" name="grade_section" value="E" required>
                                 <label for="12">E</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="F">
+                                <input type="radio" name="grade_section" value="F" required>
                                 <label for="12">F</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="G">
+                                <input type="radio" name="grade_section" value="G" required>
                                 <label for="12">G</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="H">
+                                <input type="radio" name="grade_section" value="H" required>
                                 <label for="12">H</label>
                             </div>
                         </div>
-                        <h4>Srand</h4>
+                        <h4>Strand</h4>
                         <div class="strand">
                             <div class="item">
-                                <input type="radio" name="grade_strand" value="TVL">
+                                <input type="radio" name="grade_strand" value="TVL" required>
                                 <label for="12">TVL</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_strand" value="STEM">
+                                <input type="radio" name="grade_strand" value="STEM" required>
                                 <label for="12">STEM</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_strand" value="GAS">
+                                <input type="radio" name="grade_strand" value="GAS" required>
                                 <label for="12">GAS</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="ABM">
+                                <input type="radio" name="grade_strand" value="ABM" required>
                                 <label for="12">ABM</label>
                             </div>
                             <div class="item">
-                                <input type="radio" name="grade_section" value="HUMMS">
+                                <input type="radio" name="grade_strand" value="HUMMS" required>
                                 <label for="12">HUMMS</label>
                             </div>
                         </div>
                         <div class="button">
-                            <input type="submit" value="Register">
+                            <button name="ADD_BTN">Register</button>
                         </div>
                     </form>
                 </div>
--- app/functions/add_student.php
+++ app/functions/add_student.php
@@ -2,20 +2,20 @@
 
 include_once("../database/dbconn.php");
 
-$fullname = $_POST["FULLNAME"];
-$gmail = $_POST["GMAIL"];
-$cp_number = $_POST["CP_NUMBER"];
-$hometown = $_POST["HOMETOWN"];
-$grade = $_POST["grade_level"];
-$section = $_POST["grade_section"];
-$strand = $_POST["grade_strand"];
 
 
-$add = "INSERT INTO student_info (stud_name, stud_section, stud_grade, stud_strand, stud_cp_number, stud_gmail) VALUES ('$fullname', '$section', '$grade', '$strand', '$cpnumber', '$gmail')";
 
 
-if (isset($_POST['ADD_BTN'])) {
-    if (empty($fullname)) {
-        header('location: ../add.php?error=Name is Required');
-    }
+
+
+if (isset($_POST["ADD_BTN"])) {
+    $fullname = $_POST["FULLNAME"];
+    $gmail = $_POST["GMAIL"];
+    $cp_number = $_POST["CP_NUMBER"];
+    $hometown = $_POST["HOMETOWN"];
+    $grade = $_POST["grade_level"];
+    $section = $_POST["grade_section"];
+    $strand = $_POST["grade_strand"];
+    mysqli_query($conn, "INSERT INTO student_info (stud_name, stud_gmail, stud_cp_num, stud_homwtown, stud_grade, stud_section, stud_strand) VALUES ('$fullname', '$gmail', '$cp_number', '$hometown', '$grade', '$section', '$strand')");
+    header("location: ../dashboard.php");
 }
