68e1c33327d7ac3e229b82688d3052495652319a
sphyxi4 <jeromeinfante0506@gmail.com>
2023-12-07 19:39:30 +0800

    Update add_student.php

--- app/functions/add_student.php
+++ app/functions/add_student.php
@@ -18,6 +18,18 @@ if (isset($_POST["ADD_BTN"])) {
         header("location: ../add.php?error=Name is Required");
     }else if(empty($gmail)){
         header("location: ../add.php?error=Gmail is Required");
+    }else if(empty($cp_number)){
+        header("location: ../add.php?error=Cellphone is Required");
+    }else if(empty($hometown)){
+        header("location: ../add.php?error=Hometown is Required");
+    }else if(empty($grade)){
+        header("location: ../add.php?error=Grade is Required");
+    }else if(empty($section)){
+        header("location: ../add.php?error=Section is Required");
+    }else if(empty($strand)){
+        header("location: ../add.php?error=Strand is Required");
+    }else if(!filter_var($gmail, FILTER_VALIDATE_EMAIL)){
+        header("location: ../add.php?error=Gmail is Invalid");
     }
 }
 ?>
