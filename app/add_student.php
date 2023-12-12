cbdf1d98a92c745a6bfa21fcc41f18af26c006eb
Asphyxia <jeromeinfante0506@gmail.com>
2023-12-12 17:07:32 +0800

    finally working na

--- app/add_student.php
+++ app/add_student.php
@@ -1,37 +1,36 @@
 <?php
 
-include_once("./database/dbconn.php");
+require_once("./database/dbconn.php");
 
+if (isset($_POST["add"])) {
+    $fullname = $_POST["FULLNAME"];
+    $gmail = $_POST["GMAIL"];
+    $cp_number = $_POST["CP_NUMBER"];
+    $hometown = $_POST["HOMETOWN"];
+    $grade = $_POST["grade_level"];
+    $section = $_POST["grade_section"];
+    $strand = $_POST["grade_strand"];
 
-$fullname = $_POST["FULLNAME"];
-$gmail = $_POST["GMAIL"];
-$cp_number = $_POST["CP_NUMBER"];
-$hometown = $_POST["HOMETOWN"];
-$grade = $_POST["grade_level"];
-$section = $_POST["grade_section"];
-$strand = $_POST["grade_strand"];
+    // $add = "INSERT INTO `testing` (`name`, `gmail`, `cp`, `hometown`) VALUES ('$fullname', '$gmail', '$cp_number', '$hometown')";
 
-// $add = "INSERT INTO `testing` (`name`, `gmail`, `cp`, `hometown`) VALUES ('$fullname', '$gmail', '$cp_number', '$hometown')";
-
-if (empty($fullname)) {
-    header("location:add.php?error=Name is Required");
-} else if (empty($gmail)) {
-    header("location: add.php?error=Gmail is Required");
-} else if (empty($cp_number)) {
-    header("location: add.php?error=Cellphone is Required");
-} else if (empty($hometown)) {
-    header("location: add.php?error=Hometown is Required");
-} else if (empty($grade)) {
-    header("location: add.php?error=Grade is Required");
-} else if (empty($section)) {
-    header("location: add.php?error=Section is Required");
-} else if (empty($strand)) {
-    header("location: add.php?error=Strand is Required");
-} else if (!filter_var($gmail, FILTER_VALIDATE_EMAIL)) {
-    header("location: add.php?error=Gmail is Invalid");
-} else {
-    mysqli_query($conn, "INSERT INTO `student_info` (stud_name, stud_gmail, stud_cp_number, stud_hometown, stud_grade, stud_section, stud_strand) VALUES ('$fullname', '$gmail', '$cp_number', '$hometown', '$grade', '$section', '$strand')");
-    header("location: add.php");
+    if (empty($fullname)) {
+        header("location:add.php?error=Name is Required");
+    } else if (empty($gmail)) {
+        header("location: add.php?error=Gmail is Required");
+    } else if (empty($cp_number)) {
+        header("location: add.php?error=Cellphone is Required");
+    } else if (empty($hometown)) {
+        header("location: add.php?error=Hometown is Required");
+    } else if (empty($grade)) {
+        header("location: add.php?error=Grade is Required");
+    } else if (empty($section)) {
+        header("location: add.php?error=Section is Required");
+    } else if (empty($strand)) {
+        header("location: add.php?error=Strand is Required");
+    } else if (!filter_var($gmail, FILTER_VALIDATE_EMAIL)) {
+        header("location: add.php?error=Gmail is Invalid");
+    } else {
+        mysqli_query($conn, "INSERT INTO `student` (`grade`, `section`, `strand`) VALUES ('$grade', '$section', '$strand')");
+        header("location: add.php");
+    }
 }
-
-?>
